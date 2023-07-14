1d1cac201778a151a127d2dbc5afc7cedbd8f2b1
Souvik <souvikroy1999ab@gmail.com>
2023-07-14 19:08:44 +0530

    setup CORS

--- src/main/java/com/dashboard/visualgraphdashboard/VisualGraphDashboardApplication.java
+++ src/main/java/com/dashboard/visualgraphdashboard/VisualGraphDashboardApplication.java
@@ -3,7 +3,10 @@ package com.dashboard.visualgraphdashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = "com.dashboard")
 @EnableJpaRepositories("com.dashboard.repository")
@@ -12,6 +15,20 @@ public class VisualGraphDashboardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VisualGraphDashboardApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 
 }
